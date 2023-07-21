b416653e71a225ca1d2c74b046daeb852330ce26
auliadiapari <aulia.diapari@gmail.com>
2023-07-21 11:14:25 +0700

    Initial Commits

---
+++ tests/test_login.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from pages.login_page import login_page
+
+login_page.go_to_page
+login_page.input_username
+login_page.input_password
+login_page.click_login_button
+login_page.verify_home_page
+    
